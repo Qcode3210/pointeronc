7bc770e1c15ebf4687d28d02ff190d2052eb9064
Qucode3210 <1249552018@qq.com>
2023-11-20 23:10:19 +0800

    qzy

--- findChar2.cpp
+++ findChar2.cpp
@@ -6,11 +6,11 @@
 #include <stdio.h>
 
 int findChar2(char **strings, int value) {
-	assert(strings != NULL);
+	assert(strings != NULL);//断言，程序出错时，添加诊断，输出错误信息。
 
 	while (*strings != NULL) {
 		while (**strings != '\0') {
-			if (*(*strings)++ == value) {
+			if (*(*strings)++ == value) {//else也行，不用加到判断语句中，
 				return 1;
 			}
 		}
